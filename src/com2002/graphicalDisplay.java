1e89c2caaeb285e191b0f625844f630e2ef12872
Louis Thorpe-Monaghan <lthorpe-monaghan1@sheffield.ac.uk>
2015-12-11 00:21:55 +0000

    Okay, I've redone the calendar in the netBeans UI builder which now looks much nicer. Everything is more or less layed out for use with the database, so that's good. The rest of the UI needs to be redesigned using netBeans, but that shouldn't be too difficult.

--- src/com2002/graphicalDisplay.java
+++ src/com2002/graphicalDisplay.java
@@ -34,19 +34,17 @@ public class graphicalDisplay {
     private final JPanel mainContentPane = new JPanel(new BorderLayout(3, 3));
     private final JPanel secContentPane = new JPanel(new BorderLayout(3, 3));
     // Divided up calendar
-    private JPanel calendarMaster = new JPanel();
-    private JPanel calendarMain = new JPanel();
-    private JPanel dateWrapper =  new JPanel();
-    private JPanel navWrapper =  new JPanel();
-    private JPanel calendarWrapper =  new JPanel();
+    private JPanel calendarMaster = new JPanel();   //Flow
+    private JPanel calendarMain = new JPanel();     //Box horizontal
+    private JPanel navWrapper =  new JPanel();      //Gridbag
+    private JPanel calendarWrapper =  new JPanel(); //Box horizontal
+    private JPanel dateWrapper =  new JPanel();     //Flow
+    private JPanel timetableMaster = new JPanel();  //Gridbag
+    private JPanel timeHeaders = new JPanel();      //Box horizontal
+    private JPanel hourColumns = new JPanel();      //Box horizontal
     private JButton[] dayButtons = new JButton[5];
     private Calendar calendar;
-    private int currentYear;
-    private int currentMonth;
-    private int currentDay;
-    private int viewingMonth;
-    private int viewingDay;
-    private int dayOfWeek;
+    private String dateActual;
     private BasicArrowButton nextWeek = new BasicArrowButton(BasicArrowButton.EAST);
     private BasicArrowButton prevWeek = new BasicArrowButton(BasicArrowButton.WEST);
     private JLabel weekDates;
@@ -75,7 +73,7 @@ public class graphicalDisplay {
         JButton secretary = new JButton("Secretary");
         secretary.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                secretaryMain();
+                //secretaryUI.main();
                 main.setVisible(false);
                 main.dispose();
             }
@@ -96,187 +94,6 @@ public class graphicalDisplay {
         main.setSize(screenDimensions.width/4- main.getWidth()/2,screenDimensions.height/6 - main.getWidth()/2);
         main.setLocation(new Point(screenDimensions.width/2 - main.getWidth()/2,screenDimensions.height/2 - main.getWidth()/2));
         main.setVisible(true);
-    }
-
-
-    ///////// Setup for the secretary UI //////////////////////
-    public void secretaryMain() {
-        secretaryUI = new JFrame("Secretary Calendar View");
-        secretaryUI.setContentPane(secContentPane);
-
-        ///////////////////////// SET UP CALENDAR VALUES ///////////////////////
-
-        calendar = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
-        Date now = new Date();
-        calendar.setTime(now);
-        currentYear = calendar.get(Calendar.YEAR);
-        currentMonth = calendar.get(Calendar.MONTH);
-        currentDay = calendar.get(Calendar.DAY_OF_MONTH);
-        viewingMonth = calendar.get(Calendar.MONTH)+1;
-        viewingDay = calendar.get(Calendar.DAY_OF_MONTH);
-        dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-
-
-        viewYear = new JLabel(" ", SwingConstants.CENTER);
-        viewYear.setFont(new Font("Helvetica", Font.PLAIN, 20));
-        weekDates = new JLabel(" ", SwingConstants.CENTER);
-        weekDates.setFont(new Font("Helvetica", Font.PLAIN, 20));
-        viewWeek = new JLabel(" ", SwingConstants.CENTER);
-        viewWeek.setFont(new Font("Helvetica", Font.PLAIN, 20));
-        updateNav();
-        //////////////////////////////////////////////////////////////////////////////
-
-
-
-        //Build and the CalendarMaster panel
-        calendarMaster.setLayout(new BoxLayout(calendarMaster, BoxLayout.Y_AXIS));
-        //Gridbag for the nav bar
-        navWrapper.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        //c.fill = GridBagConstraints.HORIZONTAL;
-
-        // Week scrolling Buttons
-        prevWeek.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-
-                calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                calendar.add(Calendar.DATE, -7);
-
-
-                calendarMain.removeAll();
-                dateWrapper.removeAll();
-                calendarWrapper.removeAll();
-                calendarMaster.remove(calendarMain);
-
-                buildCalendarMain(secContentPane);
-                calendarMaster.repaint();
-                calendarMaster.revalidate();
-
-
-
-                updateNav();
-                System.out.println("BackViewingDay");
-                System.out.println(viewingDay);
-
-            }
-        });
-        nextWeek.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                calendar.add(Calendar.DATE, +7);
-
-                calendarMain.removeAll();
-                dateWrapper.removeAll();
-                calendarWrapper.removeAll();
-                calendarMaster.remove(calendarMain);
-
-                buildCalendarMain(secContentPane);
-                calendarMaster.repaint();
-                calendarMaster.revalidate();
-
-
-
-                updateNav();
-                System.out.println("NextViewingDay");
-                System.out.println(viewingDay);
-            }
-        });
-        //////////////////////////////////////////////////////////////////////////
-
-        // Gridbag stuff
-        c.ipady = 30;
-        c.ipadx = 40;
-        c.gridx = 0;
-        c.gridy = 0;
-        navWrapper.add(prevWeek,c);
-        c.ipady = 30;
-        c.ipadx = 50;
-        c.gridx = 2;
-        c.gridy = 0;
-        c.gridwidth = 5;
-        navWrapper.add(weekDates,c);
-        c.ipady = 30;
-        c.ipadx = 50;
-        c.gridx = 7;
-        c.gridy = 0;
-        c.gridwidth = 4;
-        navWrapper.add(viewYear,c);
-        c.ipady = 30;
-        c.ipadx = 40;
-        c.gridx = 11;
-        c.gridy = 0;
-        navWrapper.add(nextWeek,c);
-
-        calendarMaster.add(navWrapper);
-
-
-        // build the calendar Main Panel, housing the day buttons
-        buildCalendarMain(secContentPane);
-
-        secretaryUI.add(calendarMaster);
-        secretaryUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        secretaryUI.setSize(500,300);
-        secretaryUI.setLocation(new Point(screenDimensions.width/2 - main.getWidth()/2,screenDimensions.height/2 - main.getWidth()/2));
-        secretaryUI.setVisible(true);
-    }
-
-
-    //////////////////// Setup the main calendar panel ///////////////////////////
-    public void buildCalendarMain(JPanel content) {
-
-        calendarMain.setLayout(new BoxLayout(calendarMain, BoxLayout.Y_AXIS));
-
-        //WIP
-        calendarMain.add(dateWrapper);
-
-        // Build the calendar wrapper
-        buildCalendarWrapper();
-        calendarMaster.add(calendarMain);
-
-    }
-
-    //////////////// Setup the weapper for the calendar days /////////////////////
-    public void buildCalendarWrapper(){
-
-        calendarWrapper.setLayout(new FlowLayout());
-
-        JButton currentButton;
-
-        for (int i=0; i<dayButtons.length; i++){
-            JButton day = new JButton(days[i]);
-            if (currentDay-2==i && viewingDay==currentDay){
-                day.setBackground(Color.LIGHT_GRAY);
-            }
-            else{
-                day.setBackground(Color.WHITE);
-            }
-            dayButtons[i] = day;
-        }
-
-        for (int i=0; i<dayButtons.length; i++){
-            currentButton = dayButtons[i];
-            calendarWrapper.add(currentButton);
-        }
-
-        calendarMain.add(calendarWrapper);
-
-    }
-
-
-    ///////////// Update the Nav bar ///////////////////
-    public void updateNav(){
-        DateFormat dFormat = new SimpleDateFormat("dd/MM", Locale.getDefault());
-
-        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-        weekStart = dFormat.format(calendar.getTime());
-        calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
-        weekEnd = dFormat.format(calendar.getTime());
-
-        weekDates.setText(weekStart + "-" + weekEnd);
-
-        DateFormat yFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
-        viewingYear = yFormat.format(calendar.getTime());
-        viewYear.setText(viewingYear);
     }
 
 
